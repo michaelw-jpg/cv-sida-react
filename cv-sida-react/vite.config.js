dfc8020f3cea622106929e3e503218f5131477bb
michael.wortel@chasacademy.se <michael.wortel@chasacademy.se>
2025-02-19 12:44:31 +0100

    small change to config

--- cv-sida-react/vite.config.js
+++ cv-sida-react/vite.config.js
@@ -4,13 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/cv-sida-react",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(dirname, "index.html"),
-        404: resolve(dirname, "public/404.html"),
-      },
-    },
-  },
+  base: "/cv-sida-react/",
 });
